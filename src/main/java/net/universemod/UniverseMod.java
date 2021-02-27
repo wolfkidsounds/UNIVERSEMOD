277d03204aba3a13028c122c33156170f726fa25
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2021-02-27 17:51:03 +0100

    Made OreGen compatible with Dimensional World

--- src/main/java/net/universemod/UniverseMod.java
+++ src/main/java/net/universemod/UniverseMod.java
@@ -45,7 +45,7 @@ import java.util.function.Supplier;
 @Mod(modid = UniverseMod.MODID, version = UniverseMod.VERSION)
 public class UniverseMod {
 	public static final String MODID = "universemod";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "a1.1";
 	public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("universemod:a");
 	@SidedProxy(clientSide = "net.universemod.ClientProxyUniverseMod", serverSide = "net.universemod.ServerProxyUniverseMod")
 	public static IProxyUniverseMod proxy;
